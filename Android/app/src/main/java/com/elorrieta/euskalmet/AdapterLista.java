d9407bc057feb02fed043508d8a64ba3d0e0ad33
Josu <josu.delosmozosma@elorrieta-errekamari.com>
2021-01-21 09:41:00 +0100

    Añadir favoritos terminado

--- Android/app/src/main/java/com/elorrieta/euskalmet/AdapterLista.java
+++ Android/app/src/main/java/com/elorrieta/euskalmet/AdapterLista.java
@@ -20,6 +20,7 @@ import java.util.ArrayList;
 public class AdapterLista extends RecyclerView.Adapter<AdapterLista.ViewHolderDatos>{
 
     ArrayList<String> listaDatos;
+    ArrayList<String> listaFavoritos;
     String Usuario;
 
     public AdapterLista(ArrayList<String> listaDatos, String usuario) {
@@ -38,6 +39,17 @@ public class AdapterLista extends RecyclerView.Adapter<AdapterLista.ViewHolderDa
     public void onBindViewHolder(@NonNull ViewHolderDatos holder, int position) {
         holder.asignarDatos(listaDatos.get(position));
         holder.setOnClickListeners();
+        String consulta = "SELECT nomMunicipio FROM es_favorito_mun WHERE idUser = '" + Usuario + "'";
+        try {
+            listaFavoritos = conectar_select(consulta);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        if(listaFavoritos.contains(holder.dato.getText().toString())){
+            holder.favorito.setImageResource(R.drawable.estrella_on);
+        }else{
+            holder.favorito.setImageResource(R.drawable.estrella_off);
+        }
     }
 
     @Override
@@ -53,11 +65,11 @@ public class AdapterLista extends RecyclerView.Adapter<AdapterLista.ViewHolderDa
 
         public ViewHolderDatos(@NonNull View itemView) {
             super(itemView);
-            String consulta = "SELECT";
+
             dato = (TextView) itemView.findViewById(R.id.idDato);
             favorito = (ImageView) itemView.findViewById(R.id.favorito);
 
-            favorito.setImageResource(R.drawable.estrella_off);
+
             context = itemView.getContext();
         }
 
@@ -74,7 +86,25 @@ public class AdapterLista extends RecyclerView.Adapter<AdapterLista.ViewHolderDa
         public void onClick(View v) {
             switch (v.getId()){
                 case R.id.favorito:
-                    favorito.setImageResource(R.drawable.estrella_on);
+                    if(listaFavoritos.contains(dato.getText().toString())){
+                        String consulta = "DELETE FROM es_favorito_mun WHERE idUser = '" + Usuario + "' AND nomMunicipio = '" + dato.getText().toString() + "'";
+                        try {
+                            conectar_insert(consulta);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        favorito.setImageResource(R.drawable.estrella_off);
+                        listaFavoritos.remove(listaFavoritos.indexOf(dato.getText().toString()));
+                    }else{
+                        String consulta = "INSERT INTO es_favorito_mun(idUser, nomMunicipio) VALUES('" + Usuario + "', '" + dato.getText().toString() + "')";
+                        try {
+                            conectar_insert(consulta);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        favorito.setImageResource(R.drawable.estrella_on);
+                        listaFavoritos.add(dato.getText().toString());
+                    }
                     break;
                 case R.id.idDato:
                     break;
@@ -82,12 +112,19 @@ public class AdapterLista extends RecyclerView.Adapter<AdapterLista.ViewHolderDa
         }
     }
 
-    private ArrayList conectar(String consulta) throws InterruptedException {
+    private ArrayList conectar_select(String consulta) throws InterruptedException {
         ClientThread clientThread = new ClientThread(consulta);
-        clientThread.columnaResultado = "password";
+        clientThread.columnaResultado = "nomMunicipio";
         Thread thread = new Thread(clientThread);
         thread.start();
         thread.join(); // Esperar respusta del servidor...
         return clientThread.getResponse();
+    }
+
+    private void conectar_insert(String consulta) throws InterruptedException {
+        Client_Insercion_Update clientThread = new Client_Insercion_Update(consulta);
+        Thread thread = new Thread(clientThread);
+        thread.start();
+        thread.join(); // Esperar respusta del servidor...
     }
 }
