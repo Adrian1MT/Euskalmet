feb0c3266acfb97f73a372744bcfdbfa55773cd0
Adrian1MT <adrian.martineztr@elorrieta-errekamari.com>
2021-01-23 19:33:18 +0100

    Recuperacion de mensaje

--- Android/app/src/main/java/com/elorrieta/euskalmet/EnviarCorreoRecordatorio.java
+++ Android/app/src/main/java/com/elorrieta/euskalmet/EnviarCorreoRecordatorio.java
@@ -8,6 +8,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class EnviarCorreoRecordatorio extends AppCompatActivity {
     ImageView Imagen;
@@ -26,10 +29,27 @@ public class EnviarCorreoRecordatorio extends AppCompatActivity {
         button = findViewById(R.id.BtnEnviar);
 
     }
-    public void Enviar(View v) {
+    public void Enviar(View v) throws InterruptedException {
         String enviarcorreo = correo.getText().toString();
-        String enviarasunto = Usuario.getText().toString();
+        String enviarUsuario = Usuario.getText().toString();
         String enviarmensaje = "Prueba0";
+
+        String consulta;
+        ArrayList<String> ListaRecuperacion = new ArrayList<String>();
+
+        consulta = "SELECT recuperacion FROM usuarios WHERE idUser='" + enviarUsuario.trim() +"'";
+        ListaRecuperacion = conectar(consulta);
+       // enviarmensaje=ListaRecuperacion.get(0).toString();
+
+        if(ListaRecuperacion.isEmpty()){
+            enviarmensaje="Lo sentimos no existe registros en nuestra base de datos";
+        }else {
+            enviarmensaje=ListaRecuperacion.get(0);
+            enviarmensaje=enviarmensaje.trim();
+            if(enviarmensaje.length()==0){
+                enviarmensaje="El usuario no inserto recordatorio";
+            }
+        }
 
         // Defino mi Intent y hago uso del objeto ACTION_SEND
         Intent intent = new Intent(Intent.ACTION_SEND);
@@ -37,7 +57,7 @@ public class EnviarCorreoRecordatorio extends AppCompatActivity {
         // Defino los Strings Email, Asunto y Mensaje con la función putExtra
         intent.putExtra(Intent.EXTRA_EMAIL,
                 new String[] { enviarcorreo });
-        intent.putExtra(Intent.EXTRA_SUBJECT, enviarasunto);
+        intent.putExtra(Intent.EXTRA_SUBJECT, enviarUsuario);
         intent.putExtra(Intent.EXTRA_TEXT, enviarmensaje);
 
         // Establezco el tipo de Intent
@@ -45,5 +65,14 @@ public class EnviarCorreoRecordatorio extends AppCompatActivity {
 
         // Lanzo el selector de cliente de Correo
         startActivity( Intent.createChooser(intent,"Elije un cliente de Correo:"));
+    }
+
+    private ArrayList conectar(String consulta) throws InterruptedException {
+        ClientThread clientThread = new ClientThread(consulta);
+        clientThread.columnaResultado = "recuperacion";
+        Thread thread = new Thread(clientThread);
+        thread.start();
+        thread.join(); // Esperar respusta del servidor...
+        return clientThread.getResponse();
     }
 }
