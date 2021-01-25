16094e9c727a4b08a2eeec1ba8e8428d5fee0384
Adrian1MT <adrian.martineztr@elorrieta-errekamari.com>
2021-01-25 21:56:02 +0100

    MAP

--- Android/app/src/main/java/com/elorrieta/euskalmet/Detalles_Municipios.java
+++ Android/app/src/main/java/com/elorrieta/euskalmet/Detalles_Municipios.java
@@ -1,14 +1,27 @@
 package com.elorrieta.euskalmet;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import java.util.ArrayList;
 
-public class Detalles_Municipios extends AppCompatActivity {
+public class Detalles_Municipios extends AppCompatActivity  {
     String NombreMun;
     TextView TxtMun;
     EditText Descripcion;
@@ -16,6 +29,7 @@ public class Detalles_Municipios extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.detalles__municipios);
+
         TxtMun= (TextView) findViewById(R.id.textMunicipio);
         Descripcion= (EditText) findViewById(R.id.TextDescripcion);
 
@@ -43,5 +57,42 @@ public class Detalles_Municipios extends AppCompatActivity {
         thread.start();
         thread.join(); // Esperar respusta del servidor...
         return clientThread.getResponse();
+    }
+    private ArrayList cargarLongitud(String consulta) throws InterruptedException {
+        ClientThread clientThread = new ClientThread(consulta);
+        clientThread.columnaResultado = "longitud";
+        Thread thread = new Thread(clientThread);
+        thread.start();
+        thread.join(); // Esperar respusta del servidor...
+        return clientThread.getResponse();
+    }
+    private ArrayList cargarLatitud(String consulta) throws InterruptedException {
+        ClientThread clientThread = new ClientThread(consulta);
+        clientThread.columnaResultado = "latitud";
+        Thread thread = new Thread(clientThread);
+        thread.start();
+        thread.join(); // Esperar respusta del servidor...
+        return clientThread.getResponse();
+    }
+    public void cargarmapa(View poView) throws InterruptedException {
+        Double Longitud;
+        Double Latitud;
+
+        String Consulta= "SELECT longitud FROM municipios WHERE nombre='" + NombreMun +"'";
+        ArrayList<String> ListaPosicion = new ArrayList<String>();
+        ListaPosicion = cargarLongitud(Consulta);
+        Longitud=Double.parseDouble(ListaPosicion.get(0));
+
+        ListaPosicion.clear();
+
+        Consulta= "SELECT latitud FROM municipios WHERE nombre='" + NombreMun +"'";
+        ListaPosicion = cargarLatitud(Consulta);
+        Latitud=Double.parseDouble(ListaPosicion.get(0));
+
+        Intent oIntent = new Intent(this, Mapa.class);
+        oIntent.putExtra("longitud", Longitud);
+        oIntent.putExtra("latitud", Latitud);
+        oIntent.putExtra("Nombre", NombreMun);
+        startActivity(oIntent);
     }
 }
---
+++ Android/app/src/main/java/com/elorrieta/euskalmet/Mapa.java
@@ -0,0 +1,94 @@
+package com.elorrieta.euskalmet;
+
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class Mapa extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener
+{
+    private GoogleMap mapa;
+    private LatLng LUGAR;
+    Button IR,Animado;
+    Double longitud;
+    Double latitud;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        String nombre;
+        Bundle oExtras = getIntent().getExtras();
+        longitud = oExtras.getDouble("longitud");
+
+        latitud = oExtras.getDouble("latitud");
+
+        //nombre= oExtras.getString("Nombre");
+
+        //IR=(Button) findViewById(R.id.button1);
+        //IR.setTag("Ir a"+nombre);
+        //Animado=(Button) findViewById(R.id.button2);
+        //Animado.setTag("Animado a"+nombre);
+
+        mapa();
+        setContentView(R.layout.mapa);
+        SupportMapFragment mapFragment = (SupportMapFragment)
+                getSupportFragmentManager().findFragmentById(R.id.mapa);
+        mapFragment.getMapAsync(this);
+    }
+
+    public void mapa(){
+        LUGAR = new LatLng(latitud, longitud);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mapa = googleMap;
+        mapa.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        mapa.getUiSettings().setZoomControlsEnabled(false);
+        // Oculta los controles de zoom.
+                mapa.moveCamera(CameraUpdateFactory.newLatLngZoom(LUGAR, 15));
+        //Nivel de zoom con el que queremos que entre.
+                mapa.addMarker(new MarkerOptions()
+                .position(LUGAR)
+                .title("Elorrieta")
+                .snippet("CIFP Elorrieta-Erreka Mari LHII")
+                .icon(BitmapDescriptorFactory
+                        .fromResource(android.R.drawable.ic_menu_compass))
+                .anchor(0.5f, 0.5f));
+        mapa.setOnMapClickListener(this);
+        if (ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED) {
+            mapa.setMyLocationEnabled(true);
+            mapa.getUiSettings().setCompassEnabled(true);
+        }
+    }
+    public void moveCamera(View view) {
+        mapa.moveCamera(CameraUpdateFactory.newLatLng(LUGAR));
+    }
+    public void animateCamera(View view) {
+        mapa.animateCamera(CameraUpdateFactory.newLatLng(LUGAR));
+    }
+    public void addMarker(View view) {
+        mapa.addMarker(new MarkerOptions().position(
+                mapa.getCameraPosition().target));
+    }
+    @Override
+    public void onMapClick(LatLng puntoPulsado) {
+        mapa.addMarker(new MarkerOptions().position(puntoPulsado)
+                .icon(BitmapDescriptorFactory
+                        .defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)));
+}
+}
